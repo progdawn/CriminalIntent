8b501e4408c5b464552f28f5e1d50e7032e916da
Dawn Myers <progdawn@gmail.com>
2017-05-07 13:18:46 -0500

    Chose contact's name is set at the suspect's name

--- app/src/main/java/com/progdawn/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/progdawn/criminalintent/CrimeFragment.java
@@ -3,6 +3,8 @@ package com.progdawn.criminalintent;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
@@ -68,6 +70,24 @@ public class CrimeFragment extends Fragment {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
             updateDate();
+        } else if(requestCode == REQUEST_CONTACT && data != null){
+            Uri contactUri = data.getData();
+            String[] queryFields = new String[]{ ContactsContract.Contacts.DISPLAY_NAME};
+
+            Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+
+            try{
+                if(c.getCount() == 0){
+                    return;
+                }
+
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect);
+            }finally{
+                c.close();
+            }
         }
     }
 
@@ -171,7 +191,7 @@ public class CrimeFragment extends Fragment {
         if(suspect == null){
             suspect = getString(R.string.crime_report_no_suspect);
         }else{
-            suspect = getString(R.string.crime_report_suspect);
+            suspect = getString(R.string.crime_report_suspect, suspect);
         }
 
         String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
